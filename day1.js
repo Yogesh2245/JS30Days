b58150d33f31977921b6b80f7f6f11b29c9dc260
Yogesh2245 <yrk2245@gmail.com>
2024-07-13 23:46:44 +0530

    Add files via upload
    Day01 All Task Completed

---
+++ day1.js
@@ -0,0 +1,50 @@
+// Activity No - 1
+// Task 1 
+var number = 5
+console.log(number)
+console.log(typeof number)
+
+// Task 2
+let string = "Yogesh"
+console.log(string)
+console.log(typeof string)
+
+// Activity No - 2
+// Task 3
+const boolValue =true
+console.log(boolValue)
+console.log(typeof boolValue)
+
+//Activity No - 3
+//Task 4 
+const number = 55
+const string = "Chai aur Code"
+const boolValue = true
+const object = {
+    name:"Yogesh",
+    age:"24",
+    gender:"Male",
+    qualification:"B.Tech",
+    state:"Maharashtra"
+}
+const arrayNumbers = ["one", "two", "three"];
+
+console.log("Type is "+ typeof number)
+console.log("Type is "+ typeof string)
+console.log("Type is "+ typeof boolValue)
+console.log("Type is "+ typeof object)
+console.log("Type is "+ typeof arrayNumbers)
+
+
+
+// Activity No - 4
+// Task 5 
+let Number = 100
+console.log(Number)
+Number=200
+console.log(Number)
+
+// Activity No - 5
+// Task 6
+const Number1 = 100
+Number1 = 200
